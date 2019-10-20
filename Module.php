79624299433cf754b3d482090645fc1a5e8c32b2
dave-yang <daworld.ny@gmail.com>
2019-10-20 10:31:31 +0500

    Move routes into dedicated file

--- Module.php
+++ Module.php
@@ -16,52 +16,7 @@ final class Module extends AbstractModule
      */
     public function getRoutes()
     {
-        return array(
-            // Groups
-            '/admin/groups' => array(
-                'controller' => 'Admin:Group@indexAction'
-            ),
-            '/admin/groups/add' => array(
-                'controller' => 'Admin:Group@addAction'
-            ),
-            '/admin/groups/edit/(:var)' => array(
-                'controller' => 'Admin:Group@editAction'
-            ),
-            '/admin/groups/delete/(:var)' => array(
-                'controller' => 'Admin:Group@deleteAction'
-            ),
-            '/admin/groups/save' => array(
-                'controller' => 'Admin:Group@saveAction'
-            ),
-
-            // Items
-            '/admin/items/add' => array(
-                'controller' => 'Admin:Item@addAction'
-            ),
-            '/admin/items/edit/(:var)' => array(
-                'controller' => 'Admin:Item@editAction'
-            ),
-            '/admin/items/delete/(:var)' => array(
-                'controller' => 'Admin:Item@deleteAction'
-            ),
-            '/admin/items/save' => array(
-                'controller' => 'Admin:Item@saveAction'
-            ),
-
-            // Value
-            '/admin/values/add/(:var)' => array(
-                'controller' => 'Admin:Value@addAction'
-            ),
-            '/admin/values/edit/(:var)' => array(
-                'controller' => 'Admin:Value@editAction'
-            ),
-            '/admin/values/delete/(:var)' => array(
-                'controller' => 'Admin:Value@deleteAction'
-            ),
-            '/admin/values/save' => array(
-                'controller' => 'Admin:Value@saveAction'
-            )
-        );
+        return include(__DIR__ . '/Config/routes.php');
     }
 
     /**
