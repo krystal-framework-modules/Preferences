4e96919796ece11b3d18f3b2eaa0f1a5075662cb
dave-yang <daworld.ny@gmail.com>
2019-10-20 09:40:11 +0500

    Added fetchAll() method

--- Storage/MySQL/ValueMapper.php
+++ Storage/MySQL/ValueMapper.php
@@ -23,4 +23,21 @@ final class ValueMapper extends AbstractMapper
     {
         return 'id';
     }
+
+    /**
+     * Fetch all values by associated item id
+     * 
+     * @param int $itemId
+     * @param boolean $sort Whether sorting is required
+     * @return array
+     */
+    public function fetchAll($itemId, $sort)
+    {
+        $db = $this->db->select('*')
+                       ->from(self::getTableName())
+                       ->whereEquals('item_id', $itemId)
+                       ->orderBy($sort ? 'order' : array('id' => 'DESC'));
+
+        return $db->queryAll();
+    }
 }
